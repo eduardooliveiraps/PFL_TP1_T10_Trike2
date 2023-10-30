ecc3d8983e0de117f4e61f82073e01196b26880a
Eduardo Oliveira <up202108690@fe.up.pt>
2023-10-30 12:13:37 +0000

    display_game()

--- board.pl
+++ board.pl
@@ -1,4 +1,6 @@
 
+% board(+Size, -Board)
+% Defines the initial game board for different sizes.
 board(6,[
     [blank, blank,  cell, blank, blank],
     [blank,  cell, blank,  cell, blank],
@@ -34,11 +36,14 @@ symbol(n,S) :- S='N'.
 symbol(blank,S) :- S='---'.
 symbol(cell,S) :- S='   '.
 
+% initial_state(+Size, -GameState)
+% Initializes the initial state of the game based on the given board size.
 initial_state(Size,[Board,Player]) :-
     board(Size,Board),
     GameState = [Board,Player].
 
-
+% display_column_numbering(+ColumnNumber, +TotalNumberOfColumns)
+% Displays column numbering from 1 to Max on the game board.
 display_column_numbering(Max, Max):-
     format('| ~d |', [Max]), !.
 display_column_numbering(1, Max):-
@@ -54,6 +59,8 @@ display_column_numbering(N, Max):-
     Next is N + 1,
     display_column_numbering(Next, Max).
 
+% display_line(+TotalNumberOfColumns)
+% Generates and prints a horizontal line separator for the game board.
 display_line(0):-
     write('---|\n'), !.
 display_line(N):-
@@ -61,6 +68,8 @@ display_line(N):-
     N1 is N - 1,
     display_line(N1).
 
+% display_rows(+Board, +Rows, +Columns)
+% Displays the rows of the game board with their respective elements.
 display_rows([], 0, _).
 display_rows([Line|Rest], Rows, Columns) :-
     format(' ~d |', [Rows]),
@@ -69,13 +78,16 @@ display_rows([Line|Rest], Rows, Columns) :-
     RemainingRows is Rows - 1,
     display_rows(Rest, RemainingRows,  Columns).
 
+% display_elements(+Line)
+% Displays the elements of a particular row in the game board.
 display_elements([]).
 display_elements([CurrentElement|Rest]) :-
     symbol(CurrentElement, Symbol),
     format('~w|', [Symbol]),
     display_elements(Rest).
 
-
+% display_player_turn(+Player)
+% Displays a message indicating the player who will make the next move
 display_player_turn(Player) :-
     symbol(Player, Symbol),
     format(' > Player ~w turn to play!', [Symbol]).
